b8e9d7dee838bb6ffb8d33fcc6d99107aa08d331
Matej <matej.pilekic@gmail.com>
2019-01-22 15:25:27 +0100

    CORS added

--- main.go
+++ main.go
@@ -20,8 +20,7 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-
-	handler = cors.AllowAll().Handler(router)
+	router.use(CORSMiddleware)
 
 	
 	router.POST("/keep-alive", keepAliveHandler)
@@ -144,4 +143,20 @@ func action(c *gin.Context) {
 
 	return
 
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
